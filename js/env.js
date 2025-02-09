18e4deb6097db2fdfe455faae9dcfccc7986348a
GSKarthik369 <g.s.karthik369@gmail.com>
2025-02-09 21:18:26 +0530

    update commit

--- js/env.js
+++ js/env.js
@@ -1,9 +1,11 @@
 const env = {
     brideName: "Ajin V Alex",
     groomName: "Solomon K.R",
-    weddingDate: "08th February 2025",
-    venue: "St. Mary's Malankara Syriac Catholic Church, Ayoor",
+    type: "Wedding",
+    weddingDate: "17th February 2025",
+    venue: "St. Mary's Malankara Syriac Catholic Church, Mylom East",
     time: "11:00 A.M.",
+    eventType: "are getting married",
     bestCompliments: "Best Compliments from Josun V Alex and Vadakkethalackal Family",
     invitationDownload: "Invitation | 17 February | Hope to see you there!",
     // whatsappURL: "https://api.whatsapp.com/send?phone=+919400693283&text=Hello%20Josun%20V%20Alex,%20We%20are%20joining%20your%20sister's%20wedding%20function&source=&data="
@@ -12,21 +14,22 @@ const env = {
 document.addEventListener("DOMContentLoaded", function () {
     document.querySelector("meta[property='og:title']").setAttribute(
         "content",
-        `Engagement Invitation | ${env.brideName} weds ${env.groomName} | ${env.weddingDate} | ${env.venue}`
+        `${env.type} Invitation | ${env.brideName} weds ${env.groomName} | ${env.weddingDate} | ${env.venue}`
     );
     document.querySelector("meta[name='twitter:title']").setAttribute(
         "content",
-        `Engagement Invitation | ${env.brideName} weds ${env.groomName} | ${env.weddingDate}`
+        `${env.type} Invitation | ${env.brideName} weds ${env.groomName} | ${env.weddingDate}`
     );
     document.querySelector("meta[property='og:site_name']").setAttribute(
         "content",
-        `Engagement Invitation | ${env.brideName} weds ${env.groomName} | ${env.weddingDate}`
+        `${env.type} Invitation | ${env.brideName} weds ${env.groomName} | ${env.weddingDate}`
     );
 
     document.querySelector(".title h1:first-of-type").innerText = env.brideName;
     document.querySelector(".title h1:last-of-type").innerText = env.groomName;
     document.querySelector(".title .date").innerText = env.weddingDate;
     document.querySelector(".title .place").innerText = env.venue;
+    document.querySelector(".title .event-type").innerText = env.eventType;
     document.querySelector('.time').textContent = env.time;
     document.querySelector('.dance-med2').textContent = env.bestCompliments;
     // document.querySelector('.venue-link').setAttribute('href', env.VENUE_URL);
